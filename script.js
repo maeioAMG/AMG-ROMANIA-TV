f4c0d000f62346b620141d82a90ddc9f60025d77
MARYO JOAQUIM <achimmarius1972@gmail.com>
2025-10-25 02:25:48 +0300

    Refactor channel loading to use div elements

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
+// Încarcă canalele din tv-links.json și le inserează în grilă
 fetch('tv-links.json')
   .then(response => response.json())
-  .then(data => {
-    const lista = document.getElementById('lista-canale');
-    data.forEach(canal => {
-      const item = document.createElement('li');
-      item.innerHTML = `<a href="${canal.link}" target="_blank">${canal.nume}</a>`;
-      lista.appendChild(item);
+  .then(canale => {
+    const container = document.getElementById('canale-container');
+
+    canale.forEach(canal => {
+      const div = document.createElement('div');
+      div.className = 'canal';
+      div.setAttribute('data-category', canal.categorie.toLowerCase());
+      div.setAttribute('data-name', canal.nume.toLowerCase());
+
+      div.innerHTML = `
+        <a href="${canal.link}" target="_blank">
+          <img src="${canal.sigla}" alt="${canal.nume}">
+          <span>${canal.nume}</span>
+        </a>
+      `;
+
+      container.appendChild(div);
     });
+  })
+  .catch(error => {
+    console.error('Eroare la încărcarea canalelor:', error);
   });
